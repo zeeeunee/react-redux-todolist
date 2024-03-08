6abb1f37fc742bb6469a0b448c00f384edb378da
zeeeunee <wldmstv@gmail.com>
2024-03-08 19:13:22 +0900

    slice파일 reducer에 delete함수 추가, delete버튼에 onclick이벤트 기능 추가

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { toggleComplete } from '../redux/todoSlice';
+import { toggleComplete, deleteTodo } from '../redux/todoSlice';
 
 const TodoItem = ({ id, title, completed }) => {
 	const dispatch = useDispatch();
 	const handleCompleteClick = () => {
 		dispatch(toggleComplete({ id: id, completed: !completed }));
+	};
+	const handleDeleteClick = () => {
+		dispatch(deleteTodo({ id: id }));
 	};
 	return (
 		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
@@ -14,7 +17,9 @@ const TodoItem = ({ id, title, completed }) => {
 					<input type='checkbox' className='mr-3' checked={completed} onChange={handleCompleteClick}></input>
 					{title}
 				</span>
-				<button className='btn btn-danger'>Delete</button>
+				<button onClick={handleDeleteClick} className='btn btn-danger'>
+					Delete
+				</button>
 			</div>
 		</li>
 	);
